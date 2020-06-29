737d3d84c5449604af51c90092f8b466c1fc9c8d
Sasa Radovanovic <salle90ar@gmail.com>
2020-06-29 14:07:55 +0200

    Navbar Menu

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+// Navbar Toggle
+burgerMenu = document.querySelector('.burger-menu');
+menuResponsive = document.querySelector('.menu-responsive');
+links = document.querySelectorAll('.link');
+
+const toggleNavbar = () => {
+  burgerMenu.classList.toggle('active');
+  menuResponsive.classList.toggle('active');
+};
+
+const removeNavbar = () => {
+  burgerMenu.classList.remove('active');
+  menuResponsive.classList.remove('active');
+};
+
+burgerMenu.addEventListener('click', toggleNavbar);
+links.forEach((link) => {
+  link.addEventListener('click', removeNavbar);
+});
