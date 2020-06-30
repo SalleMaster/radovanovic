7858b57367a9e8d47b64a618355f736216cc03ff
Sasa Radovanovic <salle90ar@gmail.com>
2020-06-30 15:55:41 +0200

    Hero section animation

--- js/app.js
+++ js/app.js
@@ -17,3 +17,18 @@ burgerMenu.addEventListener('click', toggleNavbar);
 links.forEach((link) => {
   link.addEventListener('click', removeNavbar);
 });
+
+// GSAP Hero Animation
+
+window.addEventListener('load', (event) => {
+  // var rule = CSSRulePlugin.getRule('.header-anim2:after'); //get the rule
+  // gsap.to(rule, { duration: 3, cssRule: { color: 'blue' } });
+
+  var tl = gsap.timeline();
+
+  tl.from('.header-anim1', { duration: 1, opacity: 0 })
+    .from('.header-anim2', { duration: 2, opacity: 0 })
+    .from('.header-anim3', { duration: 0.7, opacity: 0 })
+    .from('.header-anim4', { duration: 0.7, opacity: 0 });
+  // .from('.header-anim5', { duration: 2, opacity: 0 });
+});
